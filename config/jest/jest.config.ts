763ed0e8827d5244fe7a81df99fd5bb053c85134
Rudy Grays <Egormatveev228@inbox.ru>
2024-10-20 16:40:39 +0500

    Fix jest.config global var  __API_URL__

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -3,6 +3,7 @@ import path from "path";
 export default {
   globals: {
     __IS_DEV__: true,
+    __API_URL__: "",
   },
   coveragePathIgnorePatterns: ["\\\\node_modules\\\\"],
   clearMocks: true,
